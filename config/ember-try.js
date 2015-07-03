296ae7fd830ae3b054be283a69ed52162db22f4a
dagda1 <dagda1@scotalt.net>
2015-07-03 16:34:22 +0100

    update ember to 13.1 and ember-cli 0.27

--- config/ember-try.js
+++ config/ember-try.js
@@ -5,6 +5,18 @@ module.exports = {
       dependencies: {
         'ember': '1.11.0'
       }
+    },
+    {
+      name: 'Ember 1.12.0',
+      dependencies: {
+        'ember': '1.12.0'
+      }
+    },
+    {
+      name: 'Ember 1.13.1',
+      dependencies: {
+        'ember': '1.13.1'
+      }
     }
   ]
 };
